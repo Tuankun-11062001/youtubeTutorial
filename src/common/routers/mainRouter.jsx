172a9d6f2f29133c99e61615c0b749ee56a2ece0
June <nhoxtuanhero1@gmail.com>
2023-09-21 20:19:57 +0700

    deploy git

--- src/common/routers/mainRouter.jsx
+++ src/common/routers/mainRouter.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/home";
 import Theme2Router from "../../modules/theme2/router/theme2Router";
 import Theme3Router from "../../modules/theme3/router/theme3Router";
 
 const MainRouter = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/theme2/*" element={<Theme2Router />} />
         <Route path="/theme3/*" element={<Theme3Router />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
